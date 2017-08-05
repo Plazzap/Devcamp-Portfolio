a310ca0b84c7b5e0d188190d6826dc528aaeb62f
Jeremy Wolff <jwolff@dsdmail.net>
2017-08-05 20:38:23 +0000

    Built out initial form and portfolio call for comments along with controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = curent_user.comments.build(comment_parms)
+  end
+  
+  private
+  
+  def comment_parms
+    params.require(:comment).permit(:content)
+  end
 end
